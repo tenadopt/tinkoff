e2b10d182d94483aedc06123245b3453d5445f96
Tenadopt <yury.ignatiev@gmail.com>
2023-12-15 15:32:56 +0300

    First task work in progress

--- readfromstdin.js
+++ readfromstdin.js
@@ -1,23 +1,23 @@
-function result() {
-    // accumulator for enter data type string
-    let userInput = ''
-    process.stdin.on('data', (data) => {
-        userInput += data.toString()
-        console.log(userInput)
-    })
-    process.stdin.on('end', () => {
-        const sum = sumFun(userInput)
-        console.log(sum)
-    })
-}
-
-const sumFun = (userInput) => {
-    return userInput
-        .trim()
-        .split('\n')
-        .map(el => parseInt(el, 10))
-        .reduce((acc, curVal) => {
-            return acc + curVal
-        })}
-
-result()
+// function result() {
+//     // accumulator for enter data type string
+//     let userInput = ''
+//     process.stdin.on('data', (data) => {
+//         userInput += data.toString()
+//         console.log(userInput)
+//     })
+//     process.stdin.on('end', () => {
+//         const sum = sumFun(userInput)
+//         console.log(sum)
+//     })
+// }
+//
+// function sumFun(userInput) {
+//     return userInput
+//         .trim()
+//         .split('\n')
+//         .map(el => parseInt(el, 10))
+//         .reduce((acc, curVal) => {
+//             return acc + curVal
+//         })}
+//
+// result()
